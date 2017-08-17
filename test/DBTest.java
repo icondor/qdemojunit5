eaed706599f48af29d6d319bc7c0e389b2c34cf3
Ionel Condor <icondor@sdl.com>
2017-08-17 12:05:30 +0300

    changes

--- test/DBTest.java
+++ test/DBTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DBTest {
     @org.junit.jupiter.api.Test
     void getTaskList() {
-       assertTrue(new DB().getTaskList(2));
+       assertTrue(new DB().getTaskList(2)); // will fail in our case
     }
 
 }
